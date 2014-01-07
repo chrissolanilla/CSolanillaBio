ea344cb163ebe8bb83c70ba0b28401982f39100f
Elizabeth Williams <elizabeth.williams@ucf.edu>
2014-01-07 14:20:43 -0500

    Added tutorial thumbnail images, which can be subject to change if we don't like this style

--- tutorials.php
+++ tutorials.php
@@ -34,35 +34,35 @@
             <section id="tutorials">
                 <section class="tutitem"> 
                 	<h1><a href="http://techrangers.cdl.ucf.edu/introduction-to-canvas-html5">Introduction to HTML5 and Canvas</a></h1>
-                    <img src="#" alt="Introduction to HTML5 and Canvas" />
+                    <img src="images/tutorials/intro_html5_canvas.jpg" alt="Introduction to HTML5 and Canvas" />
                     <p>This tutorial is designed to introduce you to working with the HTML5 canvas tag and JavaScript by walking you through the creation of controls for a top down driving game.</p>
                     <p class="author">Created By: Christopher McLean on April 17<sup>th</sup>, 2013</p>
                 </section>
                 
                 <section class="tutitem">
                 	<h1><a href="http://techrangers.cdl.ucf.edu/techrangers-training">Techrangers Training</a></h1>
-                    <img src="#" alt="Techrangers Training" />
+                    <img src="images/tutorials/techrangers_training.jpg" alt="Techrangers Training" />
                     <p>This tutorial is designed to introduce you to the Fuel PHP framework by walking you through the creation of a simple messaging app.</p>
                     <p class="author">Created By the Techrangers: on November 9<sup>th</sup>, 2012</p>
                 </section>
                 
                 <section class="tutitem"> 
                 	<h1><a href="http://techrangers.cdl.ucf.edu/oauth-python-tutorial">OAuth Python Tutorial</a></h1>
-                    <img src="#" alt="OAuth Python Tutorial" />
+                    <img src="images/tutorials/oauth_python.jpg" alt="OAuth Python Tutorial" />
                     <p>This tutorial explains how to use OAuth 1.0 with Python and the Django framework.</p>
                     <p class="author">Created By: Munawar Bijani on May 2<sup>nd</sup>, 2012</p>
                 </section>
                 
                 <section class="tutitem"> 
                 	<h1><a href="http://techrangers.cdl.ucf.edu/a-simple-poll-manager-with-django">A Simple Poll Manager With Django</a></h1>
-                    <img src="#" alt="A Simple Poll Manager With Django" />
+                    <img src="images/tutorials/poll_manager_django.jpg" alt="A Simple Poll Manager With Django" />
                     <p>In this tutorial I'll be showing you how to make a simple poll manager using Django, a Python framework.</p>
                     <p class="author">Created By: Jesse Slavens on April 30<sup>th</sup>, 2012</p>
                 </section>
                 
                 <section class="tutitem"> 
                 	<h1><a href="http://techrangers.cdl.ucf.edu/fuel-crash-course">Fuel Crash Course</a></h1>
-                    <img src="#" alt="Fuel Crash Course" />
+                    <img src="images/tutorials/fuelphp_crash.jpg" alt="Fuel Crash Course" />
                     <p>This tutorial is designed to introduce you to the Fuel PHP framework by walking you through the creation of a simple messaging app.</p>
                     <p class="author">Created By: Kevin Baugh &amp; Keegan Berry on October 14<sup>th</sup>, 2011</p>
                 </section>
