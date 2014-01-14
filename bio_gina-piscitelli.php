8a826be9054b23ef33a381118b3063ea34097446
Gina Piscitelli <gina.piscitelli@ucf.edu>
2014-01-14 11:31:44 -0500

    Updated bio

--- bio_gina-piscitelli.php
+++ bio_gina-piscitelli.php
@@ -33,20 +33,23 @@
             	<img src="images/bios/gina.jpg" alt="Gina Piscitelli" />
             	
                 <h2>Degree:</h2>
-                	<p>xxxxx</p>
+                	<p>Digital Media - Web Design</p>
                 
                 <h2>Expertise:</h2>
                 	<ul>
-                    	<li>HAY</li>
+                    	<li>Front end design</li>
+                        <li>Adobe Creative Suite</li>
+                        <li>HTML and CSS</li>
                     </ul>
                     
                 <h2>Learning:</h2>
                 	<ul>
-                    	<li>xxxxx</li>
+                    	<li>Back end development</li>
+                        <li>Javascript and jQuery</li>
                     </ul>
                 
                 <h2>Biography:</h2>
-                	<p>xxxxx</p>
+                	<p>Born and raised in Southwest Florida, I've known nothing but scorching hot weather my entire life. Although I was brought up in the sunshine state, I often sought shelter inside...where the air conditioning was. Becoming a hermit and having an overactive imagination is what eventually led me into having hobbies such as teaching myself HTML and CSS, Photoshop and the likes. This quickly growing interest has since become my education (and soon to be) career path.</p>
         	</section>
         
         </section>
