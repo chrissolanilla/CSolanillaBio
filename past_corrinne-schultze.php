bbe815365b511e3b44070228bf0117f736ca5608
Corrinne <corrinne.schultze@ucf.edu>
2014-02-10 08:57:05 -0500

    Forgot to add file since it's been renamed |5|

---
+++ past_corrinne-schultze.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Corrinne Schultze - Techrangers</title>
+
+<script type="text/javascript" src="http://teach.ucf.edu/wp-content/themes/online/pulldown/online-dropdown.js?2013-12"></script>
+<script type="text/javascript" src="http://universityheader.ucf.edu/bar/js/university-header.js"></script>
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+
+</head>
+
+<body>
+
+    <?php include('header.html'); ?> <!-- header.html -->
+    
+    <main role="main" id="biomain">
+    	<!-- main content -->
+        <div id="title">
+            <h1>Meet the Techrangers</h1> 
+        </div>
+        
+        <?php include('sidebar.html'); ?> <!-- sidebar.html -->
+        
+        <section role="region" class="pagecontent" id="biopage">
+
+            <section role="region">
+            <h1 class="bioh1">Corrinne Schultze</h1>
+        
+            	<img src="images/bios/corrinne.jpg" alt="Corrinne Schultze" />
+            	
+                <h2>Degree:</h2>
+                	<p>Digital Media - Web Design B.A.</p>
+
+                <h2>Websites:</h2>
+                    <ul>
+                        <li><a href="http://cschultze.github.io/" target="_blank">http://cschultze.github.io/</a></li>
+                        <li><a href="http://www.galacticlacquer.com/" target="_blank">http://www.galacticlacquer.com/</a></li>
+                    </ul>
+                
+                <h2>Expertise:</h2>
+                	<ul>
+                    	<li>HTML</li>
+                        <li>CSS</li>
+                    </ul>
+                    
+                <h2>Learning:</h2>
+                	<ul>
+                    	<li>jQuery/Javascript</li>
+                        <li>PHP</li>
+                        <li>mySQL</li>
+                    </ul>
+                
+                <h2>Biography:</h2>
+                	<p>I graduated from the University of Central Florida with a bachelor's degree in Digital Media in 2013. My focus is on web design and front end development. While I love implementing the ideas of others, I hope to also get better at design and also learn more back end development. I like to draw, bake, play video games, and do nail art for my blog, Galactic Lacquer, in my spare time.</p>
+
+                <p class="updated">Page last updated January 29<sup>th</sup>, 2014</p>
+        	</section>
+        
+        </section>
+    </main>
+    
+	<?php include('footer.html'); ?> <!-- footer.html -->
+
+</body>
+</html>
