1759a1f2aac276d63a2517b1cfa2659965df7a75
Fenel Joseph <fenel.joseph@knights.ucf.edu>
2014-08-12 10:48:20 -0400

    Add Fenel to TR Website

---
+++ bio_fenel-joseph.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Fenel Joseph - Techrangers</title>
+
+<script type="text/javascript" src="http://teach.ucf.edu/wp-content/themes/online/pulldown/online-dropdown.js?2013-12"></script>
+<script type="text/javascript" src="http://universityheader.ucf.edu/bar/js/university-header.js"></script>
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+
+</head>
+
+<body>
+
+    <?php include('header.html'); ?> <!-- header.html -->
+    
+    <main role="main" id="biomain">
+        <!-- main content -->
+        <div id="title">
+            <h1>Meet the Techrangers</h1> 
+        </div>
+        
+        <?php include('sidebar.html'); ?> <!-- sidebar.html -->
+        
+        <section role="region" class="pagecontent" id="biopage">
+            
+            <section role="region">
+            <h1 class="bioh1">Fenel Joseph</h1>
+        
+                <img src="images/bios/fenel.jpg" alt="Fenel Joseph" />
+                
+                <h2>Degree:</h2>
+                    <p>Computer Engineering (In Progress)</p>
+                    <p>Electrical Engineering (In Progress)</p>
+                
+                <h2>Website:</h2>
+                    <a href="http://www.feneljoseph.com">http://www.FenelJoseph.com</a>
+
+                <h2>Expertise:</h2>
+                    <ul>
+                        <li>Adobe Illustrator</li>
+                        <li>Adobe Photoshop</li>
+                        <li>C</li>
+                        <li>CSS 3</li>
+                        <li>HTML 5</li>
+                        <li>jQuery/Javascript</li>
+                    </ul>
+                    
+                <h2>Learning:</h2>
+                    <ul>
+                        <li>Java</li>
+                        <li>PHP</li>
+                        <li>Python</li>
+                        <li>Swift</li>
+                        <li>&amp; Everything else..</li>
+                    </ul>
+                
+                <h2>Biography:</h2>
+                    <p>I am a third-year undergraduate student matriculating at the University of Central Florida, in
+                     Orlando, Fl. I intend to complete my undergraduate coursework (GRADUATE!) by May 2016.</p> 
+                    <br />
+                    <p>With all that I am learning while in school, I hope to contribute to this technology-enabled world.
+                     How will I do this? Simple. Learn what it takes to surpass every obstacle as they come along... and
+                     then some.</p>
+            
+                <p class="updated">Page last updated August 12<sup>th</sup>, 2014</p>
+            </section>
+        
+        </section>
+    </main>
+    
+    <?php include('footer.html'); ?> <!-- footer.html -->
+
+</body>
+</html>
--- home.php
+++ home.php
@@ -47,6 +47,7 @@
                 <div class="trpic"><a href="bio_jacob-bates.php"><img src="images/thumbnails/jacob.jpg" /><p>Jacob Bates</p></a></div>
                 <div class="trpic"><a href="bio_philip-carter.php"><img src="images/thumbnails/phil.jpg" /><p>Philip Carter</p></a></div>
                 <div class="trpic"><a href="bio_ben-paulson.php"><img src="images/thumbnails/ben.jpg" /><p>Ben Paulson</p></a></div>
+                <div class="trpic"><a href="bio_fenel-joseph.php"><img src="images/thumbnails/fenel.jpg" /><p>Fenel Joseph</p></a></div>
             </div>
             
             <h2>Past Techrangers</h2>
