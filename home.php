4694e43a1aeb6320c9d522a1d46fb8b676430dcf
Eric Colon <Colon@ucf.edu>
2014-08-12 16:17:12 -0400

    added favicon, robots.txt, fixed doctype errors

--- home.php
+++ home.php
@@ -1,41 +1,41 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<title>Home - Techrangers</title>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title>Home - Techrangers</title>
 
-<script type="text/javascript" src="http://teach.ucf.edu/wp-content/themes/online/pulldown/online-dropdown.js?2013-12"></script>
-<script type="text/javascript" src="http://universityheader.ucf.edu/bar/js/university-header.js"></script>
+        <script type="text/javascript" src="http://teach.ucf.edu/wp-content/themes/online/pulldown/online-dropdown.js?2013-12"></script>
+        <script type="text/javascript" src="http://universityheader.ucf.edu/bar/js/university-header.js"></script>
 
-<link rel="stylesheet" type="text/css" href="css/reset.css">
-<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" type="text/css" href="css/styles.css">
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
 
-<script type="text/javascript">document.createElement('main');</script>
+        <script type="text/javascript">document.createElement('main');</script>
+    </head>
 
-</head>
-
-<body>
+    <body>
 
     <?php include('header.html'); ?> <!-- header.html -->
-    
+
     <main role="main" id="homemain">
-    	<!-- main content -->
+        <!-- main content -->
         <div id="title">
             <h1>Meet the Techrangers</h1> 
         </div>
-        
+
         <?php include('sidebar.html'); ?> <!-- sidebar.html -->
-        
+
         <section role="region" class="pagecontent" id="homepage">
 
             <p class="homep">The Techrangers&reg; is a team of students at the <a href="http://cdl.ucf.edu/" target="_blank">Center for Distributed Learning</a>, who provide course development for faculty and web/application development for the University of Central Florida (UCF). The Techrangers&reg; act as student advocates and leaders by holding community workshops, participating in panels and presenting at conventions. Additionally, the team provides support, troubleshooting, and consulting for various groups within the University on a variety of areas including but not limited to universal design, accessibility compliance, website design, dynamic website development requiring databases and handling of business transactions among other areas of expertise.</p>
-            
+
             <p class="homep">Feel free to <a href="mailto:techrangers@ucf.edu">contact us</a>, <a href="about.php">read about us</a>, <a href="https://plus.google.com/100247024936649114253/posts" target="_blank">network with us</a>, <a href="techtime.php">learn from us</a> and <a href="joinus.php">possibly join us</a>!</p> 
-            
+
             <p class="homep"><strong>For technical support</strong>, please contact Online@UCF Support at (407) 823-0407 or <a href="http://learn.ucf.edu/support/get-help/">http://learn.ucf.edu/support/get-help</a>. There are as well many self help online course resources on the <a href="http://learn.ucf.edu/">Learning Online Website</a>.</p>
-            
+
             <div id="photogrid">
                 <div class="trpic"><a href="bio_sevena-skeels.php"><img src="images/thumbnails/sevena.jpg" /><p>Sevena Skeels</p></a></div>
                 <div class="trpic"><a href="bio_elise-heron.php"><img src="images/thumbnails/elise.jpg" /><p>Elise Heron</p></a></div>
@@ -50,13 +50,13 @@
                 <div class="trpic"><a href="bio_fenel-joseph.php"><img src="images/thumbnails/fenel.jpg" /><p>Fenel Joseph</p></a></div>
                 <div class="trpic"><a href="bio_adrian-goetz.php"><img src="images/thumbnails/adrian.jpg" /><p>Adrian Goetz</p></a></div>
             </div>
-            
+
             <h2>Past Techrangers</h2>
-            	<p><a href="pastbios.php">Click here to view past Techrangers' biographies</a>.</p>
+                <p><a href="pastbios.php">Click here to view past Techrangers' biographies</a>.</p>
         </section>
     </main>
-    
-	<?php include('footer.html'); ?> <!-- footer.html -->
 
-</body>
+    <?php include('footer.html'); ?> <!-- footer.html -->
+
+    </body>
 </html>
