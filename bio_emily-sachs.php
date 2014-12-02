c0d5db7620e4c01d3b291d7838e8ed41104a79a7
Emily Sachs <emily.sachs@ucf.edu>
2014-12-02 11:59:07 -0500

    Added Emily Bio

---
+++ bio_emily-sachs.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include('includes.html'); ?><!-- includes.html -->
+<title>Emily Sachs - Techrangers</title>
+</head>
+
+<body>
+
+	<?php include('header.html'); ?> <!-- header.html -->
+	
+	<main role="main" id="biomain">
+		<!-- main content -->
+		<div id="title">
+			<h1>Meet the Techrangers</h1> 
+		</div>
+		
+		<?php include('sidebar.html'); ?> <!-- sidebar.html -->
+		
+		<section role="region" class="pagecontent" id="biopage">
+			
+			<section role="region">
+			<h1 class="bioh1">Emily Sachs</h1>
+		
+				<img src="images/bios/emily.jpg" alt="Emily Sachs" />
+				
+				<h2>Degree:</h2>
+					<p>Computer Science</p>
+				
+				<h2>Expertise:</h2>
+					<ul>
+						<li>HTML 5</li>
+						<li>CSS 3</li>
+						<li>Adobe Photoshop</li>
+					</ul>
+					
+				<h2>Learning:</h2>
+					<ul>
+						<li>JavaScript</li>
+						<li>Python</li>
+						<li>PHP</li>
+					</ul>
+				
+				<h2>Biography:</h2>
+					<p>I'm a second year student at UCF studying Computer Science with a minor in Digital Media. Spending way too much time on the internet led me to start teaching myself HTML, CSS, and Photoshop. I'm thrilled to be learning new web development skills! My other interests include going to concerts, space exploration, and making YouTube videos. </p>
+			
+				<p class="updated">Page last updated December 2<sup>nd</sup>, 2014</p>
+			</section>
+		
+		</section>
+	</main>
+	
+	<?php include('footer.html'); ?> <!-- footer.html -->
+
+</body>
+</html>
