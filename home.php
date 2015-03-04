257cd51627feae264e06669dbecf9da17cb15acf
Eric Colon <colon@ucf.edu>
2015-03-04 14:52:40 -0500

    rest in rip eric, 1973 - 2017 ;_;7

--- home.php
+++ home.php
@@ -30,7 +30,6 @@
 				<div class="trpic"><a href="bio_munawar-bijani.php"><img src="images/thumbnails/munawar.jpg" /><p>Munawar Bijani</p></a></div>
 				<div class="trpic"><a href="bio_matthew-emond.php"><img src="images/thumbnails/matt.jpg" /><p>Matthew Emond</p></a></div>
 				<div class="trpic"><a href="bio_elise-heron.php"><img src="images/thumbnails/elise.jpg" /><p>Elise Heron</p></a></div>
-				<div class="trpic"><a href="bio_eric-colon.php"><img src="images/thumbnails/eric.jpg" /><p>Eric Colon</p></a></div>
 				<div class="trpic"><a href="bio_gina-piscitelli.php"><img src="images/thumbnails/gina.jpg" /><p>Gina Piscitelli</p></a></div>
 				<div class="trpic"><a href="bio_fenel-joseph.php"><img src="images/thumbnails/fenel.jpg" /><p>Fenel Joseph</p></a></div>
 				<div class="trpic"><a href="bio_shauna-mason.php"><img src="images/thumbnails/shauna.jpg" /><p>Shauna Mason</p></a></div>
