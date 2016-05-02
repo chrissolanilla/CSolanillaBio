d03b598d14ff7ff545ac36897f26689fa257b343
Ryan Eppers <ry763843@cdl215346.local>
2016-05-02 13:06:46 -0400

    oops fixed file name

---
+++ bio_ryan-eppers.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include('includes.html'); ?><!-- includes.html -->
+<title>Ryan Eppers - Techrangers</title>
+</head>
+
+<body>
+
+    <?php include('header.html'); ?> <!-- header.html -->
+
+    <main role="main" id="biomain">
+    	<!-- main content -->
+        <div id="title">
+            <h1>Meet the Techrangers</h1>
+        </div>
+
+        <?php include('sidebar.html'); ?> <!-- sidebar.html -->
+
+        <section role="region" class="pagecontent" id="biopage">
+
+            <section role="region">
+            <h1 class="bioh1">Ryan Eppers</h1>
+
+            	<img src="images/bios/dan.jpg" alt="Ryan Eppers" />
+
+                <h2>Degree:</h2>
+                	<p>Information Technology</p>
+
+                <h2>Expertise:</h2>
+                	<ul>
+                        <li>Python</li>
+                        <li>Javascript</li>
+                        <li>HTML</li>
+                    </ul>
+
+                <h2>Learning:</h2>
+                	<ul>
+                        <li>Big Data</li>
+                        <li>C/C++</li>
+                        <li>CSS</li>
+                    </ul>
+
+                <h2>Biography:</h2>
+                	<p>I am an Information Technology Major at the University of Central Florida</p>
+
+                <p class="updated">Page last updated April 29<sup>th</sup>, 2016</p>
+            </section>
+        
+        </section>
+    </main>
+
+	<?php include('footer.html'); ?> <!-- footer.html -->
+</body>
+</html>
