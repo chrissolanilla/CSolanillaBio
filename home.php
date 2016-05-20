e84c806a023b229878db1354591dd5f4046e606e
Jacob Bates <jacob.bates@ucf.edu>
2016-05-20 11:14:14 -0400

    Added team photo to home page.  Fixes #29

--- home.php
+++ home.php
@@ -18,7 +18,8 @@
 		<?php include('sidebar.html'); ?> <!-- sidebar.html -->
 
 		<section role="region" class="pagecontent" id="homepage">
-
+			<div id="team-photo"></div>
+			
 			<p class="homep">The Techrangers&reg; is a team of students at the <a href="https://online.ucf.edu/about" target="_blank">Center for Distributed Learning</a>, who provide course development for faculty and web/application development for the University of Central Florida (UCF). The Techrangers&reg; act as student advocates and leaders by holding community workshops, participating in panels and presenting at conventions. Additionally, the team provides support, troubleshooting, and consulting for various groups within the University on a variety of areas including but not limited to universal design, accessibility compliance, website design, dynamic website development requiring databases and handling of business transactions among other areas of expertise.</p>
 
 			<p class="homep">Feel free to <a href="mailto:techrangers@ucf.edu">contact us</a>, <a href="about.php">read about us</a>, <a href="https://plus.google.com/100247024936649114253/posts" target="_blank">network with us</a>, <a href="techtime.php">learn from us</a> and <a href="joinus.php">possibly join us</a>!</p>
