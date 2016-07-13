b7fb879dc972d9f34abc9f98dcd40fd12bb5ed4e
William Funk <wfunk@knights.ucf.edu>
2016-07-13 16:02:45 -0400

    Final spelling and grammar edits.

--- bio_william-funk.php
+++ bio_william-funk.php
@@ -31,14 +31,15 @@
 				<h2>Experience:</h2>
 					<ul>
 						<li>HTML</li>
-						<li>CSS + (Responsive Design/Bootstrap)</li>
-						<li>Javascript + (Vanilla/JQuery/React/Angular)</li>
-						<li>PHP + (Wordpress)</li>
-						<li>C/C# + (Visual Studios/IntelliJ/Eclipse/Netbeans)</li>
+						<li>CSS (Responsive Design/Bootstrap)</li>
+						<li>Javascript (Vanilla/JQuery/React/Angular)</li>
+						<li>PHP (Wordpress)</li>
+						<li>C/C# (Visual Studios/IntelliJ/Eclipse/Netbeans)</li>
 						<li>Node (Gulp/Grunt/NPM)</li>
 						<li>Git (Github)</li>
 						<li>SQL</li>
 						<li>Dynamic Programming (Backtracking/Graph Theory)</li>
+						<li>ThreeJS</li>
 					</ul>
 
 				<h2>Learning:</h2>
@@ -49,7 +50,7 @@
 					</ul>
 
 				<h2>Biography:</h2>
-					<p>I started my career in the industry as a freelance web developer and content creator. These contracts came from companies in the US and Canada. Later, I pursued other areas of the programming profession at CDL's own New Media team, making educational widgets and midifying their development platform. In addition, I've taken on freelance contracts in game development.<br/><br/>Outside of the programming realm, I've served as a police officer, obtained a degree in Civil Engineering, backpacked through more than twenty countries, and have stories professionally published in a variety of places. I enjoy reading, writing, running, travelling, video games, and above all...coding things into existence.</p>
+					<p>I started my career in the industry as a freelance web developer and content creator. These contracts came from companies in the US and Canada. Later, I pursued other areas of the programming profession at CDL's own New Media team (May 2015), making educational widgets and midifying their development platform. In addition, I've taken on freelance contracts in game development.<br/><br/>Outside of the programming realm I've served as a police officer, obtained a degree in Civil Engineering, backpacked through more than twenty countries, and have stories professionally published in a variety of places. I enjoy reading, writing, running, traveling, video games, and above all...coding things into existence.</p>
 
 				<p class="updated">Page last updated July 13<sup>th</sup>, 2016</p>
 			</section>
