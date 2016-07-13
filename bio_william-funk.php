500752e90ef668c2df93c3b0971dab216a1edc4c
William Funk <wfunk@knights.ucf.edu>
2016-07-13 09:34:05 -0400

    Initial changes made.

---
+++ bio_william-funk.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include('includes.html'); ?><!-- includes.html -->
+<title>William Funk - Techrangers</title>
+</head>
+
+<body>
+
+    <?php include('header.html'); ?> <!-- header.html -->
+    
+    <main role="main" id="biomain">
+    	<!-- main content -->
+        <div id="title">
+            <h1>Meet the Techrangers</h1> 
+        </div>
+        
+        <?php include('sidebar.html'); ?> <!-- sidebar.html -->
+        
+        <section role="region" class="pagecontent" id="biopage">
+            
+            <section role="region">
+            <h1 class="bioh1">William Funk</h1>
+        
+            	<img src="images/bios/funk.jpg" alt="William Funk" />
+            	
+                <h2>Degree:</h2>
+                	<p>Computer Science B.S. (December 2016)</p>
+                    <p>Civil Engineering B.S.</p>
+                
+                <h2>Experience:</h2>
+                	<ul>
+                    	<li>HTML</li>
+                        <li>CSS + (Responsive Design/Bootstrap)</li>
+                        <li>Javascript + (Vanilla/JQuery/React/Angular)</li>
+                        <li>PHP + (Wordpress)</li>
+                        <li>C/C# + (Visual Studios/IntelliJ/Eclipse/Netbeans)</li>
+                        <li>Node (Gulp/Grunt/NPM)</li>
+                        <li>SQL</li>
+                        <li>Dynamic Programming (Backtracking/Graph Theory)</li>
+                    </ul>
+                    
+                <h2>Learning:</h2>
+                	<ul>
+                    	<li>Node</li>
+                        <li>CSS</li>
+                        <li>Photoshop</li>
+                    </ul>
+                
+                <h2>Biography:</h2>
+                	<p>I started my career with CDL's own New Media team developing custom Flash pieces and PHP-based web applications. After graduating in 2006, I spent a few years in private industry doing web development, marketing, and multimedia development. I have returned to UCF to become the Techrangers Team Lead. I enjoy photography, marksmanship, playing video games, and being a know-it-all.</p>
+        	
+                <p class="updated">Page last updated July 13<sup>th</sup>, 2016</p>
+            </section>
+        
+        </section>
+    </main>
+    
+	<?php include('footer.html'); ?> <!-- footer.html -->
+
+</body>
+</html>
