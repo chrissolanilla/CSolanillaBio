c0dc03bbd37e6e30619d9e825b5fcb9766ba92a4
Samuel Belcastro <belcastro.samuel@knights.ucf.edu>
2016-10-11 15:13:18 -0400

    Fixed tabbing issues

--- home.php
+++ home.php
@@ -36,7 +36,7 @@
 				<div class="trpic"><a href="bio_jesse-mcbride.php"><img src="images/thumbnails/jessemcbride.jpg" /><p>Jesse McBride</p></a></div>
 				<div class="trpic"><a href="bio_alex-nagel.php"><img src="images/thumbnails/alexn.jpg" /><p>Alex Nagel</p></a></div>
 				<div class="trpic"><a href="bio_ryan-eppers.php"><img src="images/thumbnails/ryan.jpg" /><p>Ryan Eppers</p></a></div>
-        <div class="trpic"><a href="bio_samuel-belcastro.php"><img src="images/thumbnails/samb.jpg" /><p>Samuel Belcastro</p></a></div>
+				<div class="trpic"><a href="bio_samuel-belcastro.php"><img src="images/thumbnails/samb.jpg" /><p>Samuel Belcastro</p></a></div>
 				<div class="trpic"><a href="bio_jacob-bates.php"><img src="images/thumbnails/jacob.jpg" /><p>Jacob Bates</p></a></div>
 				<div class="trpic"><a href="bio_philip-carter.php"><img src="images/thumbnails/phil.jpg" /><p>Philip Carter</p></a></div>
 			</div>
