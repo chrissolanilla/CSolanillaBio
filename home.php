e14be969201766ee8d68d4ea4fda7b806a7ac4d7
Samuel Belcastro <belcastro.samuel@knights.ucf.edu>
2016-10-11 15:05:59 -0400

    Corrected homepage to include php

--- home.php
+++ home.php
@@ -1,47 +1,13 @@
-
 <!DOCTYPE html>
 <html>
 	<head>
-				<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" /><script type="text/javascript">window.NREUM||(NREUM={}),__nr_require=function(e,t,n){function r(n){if(!t[n]){var o=t[n]={exports:{}};e[n][0].call(o.exports,function(t){var o=e[n][1][t];return r(o||t)},o,o.exports)}return t[n].exports}if("function"==typeof __nr_require)return __nr_require;for(var o=0;o<n.length;o++)r(n[o]);return r}({1:[function(e,t,n){function r(e,t){return function(){o(e,[(new Date).getTime()].concat(a(arguments)),null,t)}}var o=e("handle"),i=e(2),a=e(3);"undefined"==typeof window.newrelic&&(newrelic=NREUM);var u=["setPageViewName","setCustomAttribute","finished","addToTrace","inlineHit"],c=["addPageAction"],f="api-";i(u,function(e,t){newrelic[t]=r(f+t,"api")}),i(c,function(e,t){newrelic[t]=r(f+t)}),t.exports=newrelic,newrelic.noticeError=function(e){"string"==typeof e&&(e=new Error(e)),o("err",[e,(new Date).getTime()])}},{}],2:[function(e,t,n){function r(e,t){var n=[],r="",i=0;for(r in e)o.call(e,r)&&(n[i]=t(r,e[r]),i+=1);return n}var o=Object.prototype.hasOwnProperty;t.exports=r},{}],3:[function(e,t,n){function r(e,t,n){t||(t=0),"undefined"==typeof n&&(n=e?e.length:0);for(var r=-1,o=n-t||0,i=Array(0>o?0:o);++r<o;)i[r]=e[t+r];return i}t.exports=r},{}],ee:[function(e,t,n){function r(){}function o(e){function t(e){return e&&e instanceof r?e:e?u(e,a,i):i()}function n(n,r,o){e&&e(n,r,o);for(var i=t(o),a=l(n),u=a.length,c=0;u>c;c++)a[c].apply(i,r);var s=f[g[n]];return s&&s.push([m,n,r,i]),i}function p(e,t){w[e]=l(e).concat(t)}function l(e){return w[e]||[]}function d(e){return s[e]=s[e]||o(n)}function v(e,t){c(e,function(e,n){t=t||"feature",g[n]=t,t in f||(f[t]=[])})}var w={},g={},m={on:p,emit:n,get:d,listeners:l,context:t,buffer:v};return m}function i(){return new r}var a="nr@context",u=e("gos"),c=e(2),f={},s={},p=t.exports=o();p.backlog=f},{}],gos:[function(e,t,n){function r(e,t,n){if(o.call(e,t))return e[t];var r=n();if(Object.defineProperty&&Object.keys)try{return Object.defineProperty(e,t,{value:r,writable:!0,enumerable:!1}),r}catch(i){}return e[t]=r,r}var o=Object.prototype.hasOwnProperty;t.exports=r},{}],handle:[function(e,t,n){function r(e,t,n,r){o.buffer([e],r),o.emit(e,t,n)}var o=e("ee").get("handle");t.exports=r,r.ee=o},{}],id:[function(e,t,n){function r(e){var t=typeof e;return!e||"object"!==t&&"function"!==t?-1:e===window?0:a(e,i,function(){return o++})}var o=1,i="nr@id",a=e("gos");t.exports=r},{}],loader:[function(e,t,n){function r(){if(!w++){var e=v.info=NREUM.info,t=s.getElementsByTagName("script")[0];if(e&&e.licenseKey&&e.applicationID&&t){c(l,function(t,n){e[t]||(e[t]=n)});var n="https"===p.split(":")[0]||e.sslForHttp;v.proto=n?"https://":"http://",u("mark",["onload",a()],null,"api");var r=s.createElement("script");r.src=v.proto+e.agent,t.parentNode.insertBefore(r,t)}}}function o(){"complete"===s.readyState&&i()}function i(){u("mark",["domContent",a()],null,"api")}function a(){return(new Date).getTime()}var u=e("handle"),c=e(2),f=window,s=f.document;NREUM.o={ST:setTimeout,CT:clearTimeout,XHR:f.XMLHttpRequest,REQ:f.Request,EV:f.Event,PR:f.Promise,MO:f.MutationObserver},e(1);var p=""+location,l={beacon:"bam.nr-data.net",errorBeacon:"bam.nr-data.net",agent:"js-agent.newrelic.com/nr-943.min.js"},d=window.XMLHttpRequest&&XMLHttpRequest.prototype&&XMLHttpRequest.prototype.addEventListener&&!/CriOS/.test(navigator.userAgent),v=t.exports={offset:a(),origin:p,features:{},xhrWrappable:d};s.addEventListener?(s.addEventListener("DOMContentLoaded",i,!1),f.addEventListener("load",r,!1)):(s.attachEvent("onreadystatechange",o),f.attachEvent("onload",r)),u("mark",["firstbyte",a()],null,"api");var w=0},{}]},{},["loader"]);</script>
-
-		<script type="text/javascript">document.createElement('main');</script>
-		<script type="text/javascript" src="//universityheader.ucf.edu/bar/js/university-header.js"></script>
-
-		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-		<link rel="stylesheet" type="text/css" href="css/reset.css">
-		<link href='//fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" type="text/css" href="css/styles.css"><!-- includes.html -->
+		<?php include('includes.html'); ?><!-- includes.html -->
 		<title>Home - Techrangers</title>
 	</head>
 
 	<body>
 
-	<header role="banner">
-    <nav role="navigation">
-        <ul class="navlist">
-            <li><a href="home.php" id="techrangers">
-                <span id="char1">T</span>
-                <span id="char2">E</span>
-                <span id="char3">C</span>
-                <span id="char4">H</span>
-                <span id="char5">R</span>
-                <span id="char6">A</span>
-                <span id="char7">N</span>
-                <span id="char8">G</span>
-                <span id="char9">E</span>
-                <span id="char10">R</span>
-                <span id="char11">S</span>
-            </a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="showcase.php">Showcase</a></li>
-            <li><a href="techtime.php">Tech Time</a></li>
-            <li><a href="tutorials.php">Tutorials</a></li>
-            <li><a href="joinus.php">Join Us</a></li>
-        </ul>
-    </nav>
-</header>
- <!-- header.html -->
+	<?php include('header.html'); ?> <!-- header.html -->
 
 	<main role="main" id="homemain">
 		<!-- main content -->
@@ -49,22 +15,7 @@
 			<h1>Meet the Techrangers</h1>
 		</div>
 
-		<aside role="complementary" id="homeaside">
-    <h2>Tutorials</h2>
-        <p>Are you as eager to learn as we are? Check out some of our <a href="tutorials.php">tutorials</a>.</p>
-
-        <ol>
-            <li><a href="http://ucfcdl.github.io/html5-tutorial/" target="_blank">Introduction to HTML5 and Canvas</a></li>
-            <li><a href="techrangers-training.php">Techrangers Training</a></li>
-            <li><a href="oauth-python-tutorial.php">OAuth Python Tutorial</a></li>
-            <li><a href="a-simple-poll-manager-with-django.php">A Simple Poll Manager With Django</a></li>
-            <li><a href="http://ucf.github.io/fuelphp-crash-course/" target="_blank">Fuel Crash Course</a></li>
-        </ol>
-
-    <h2>Tech Time</h2>
-        <p>Check out our <a href="techtime.php">upcoming Tech Times</a>!</p>
-</aside>
- <!-- sidebar.html -->
+		<?php include('sidebar.html'); ?> <!-- sidebar.html -->
 
 		<section role="region" class="pagecontent" id="homepage">
 			<div id="team-photo"></div>
@@ -95,18 +46,7 @@
 		</section>
 	</main>
 
-	<footer role="contentinfo">
-    <p>Copyright &copy; 2014, Techrangers&trade; <a href="https://online.ucf.edu/about" target="_blank">Center for Distributed Learning</a></p>
+	<?php include('footer.html'); ?> <!-- footer.html -->
 
-    <div id="socmed">
-        <ul>
-            <li><a href="https://twitter.com/techrangers" id="twitter" target="_blank"><span>Twitter</span></a></li>
-            <li><a href="https://plus.google.com/u/0/100247024936649114253/posts" id="gplus" target="_blank"><span>Google Plus</span></a></li>
-            <li><a href="https://www.facebook.com/UCFTechrangers" id="facebook" target="_blank"><span>Facebook</span></a></li>
-        </ul>
-    </div>
-</footer>
- <!-- footer.html -->
-
-	<script type="text/javascript">window.NREUM||(NREUM={});NREUM.info={"beacon":"bam.nr-data.net","licenseKey":"2660c64d59","applicationID":"15579828","transactionName":"b1VaMEBYXEVUUE0LV1YfbRZbFlpZWFYXElBI","queueTime":0,"applicationTime":0,"atts":"QxJZRghCT0s=","errorBeacon":"bam.nr-data.net","agent":""}</script></body>
+	</body>
 </html>
