9f674fd6395fbfea428aef741469403ab6096480
Alex Nagel <alexander.nagel@ucf.edu>
2017-03-20 13:29:10 -0400

    Updated headhsot and bio for Alex Nagel

--- bio_alex-nagel.php
+++ bio_alex-nagel.php
@@ -21,32 +21,38 @@
                 <section role="region">
                      <h1 class="bioh1">Alex Nagel</h1>
 
-                    <img src="images/bios/alexn.jpg" alt="Alex Nagel" />
+                    <img src="images/bios/Alex Nagel.jpg" alt="Alex Nagel" />
                      <h2>Degree:</h2>
 
                     <p>Information Technologies and Information Securities</p>
                      <h2>Expertise:</h2>
 
                     <ul>
+                        <li>Security</li>
+                        <li>Python</li>
+                        <li>Javascript</li>
                         <li>HTML 5</li>
-                        <li>C++ Programming</li>
-                        <li>C Programming</li>
+                        <li>CSS 3</li>
+                        <li>C++</li>
+                        <li>C</li>
                     </ul>
                      <h2>Learning:</h2>
 
                     <ul>
-                        <li>CSS 3</li>
+                        <li>AWS Lambda</li>
+                        <li>Django</li>
+                        <li>R</li>
+                        <li>Security+</li>
                         <li>PHP</li>
-                        <li>Python</li>
                         <li>Java</li>
-                        <li>Javascript</li>
                     </ul>
                      <h2>Biography:</h2>
 
-                    <p>I'm a UCF student (junior) finishing my undergrad degree in information technologies with a minor in information securities, I will then continue on with my masters in Digital Forensics.</p>
-                    <p>Originally from South Florida. I have had a lot of past job experience having worked in a resturaunt, for the ymca and designing websites for a few clients.</p>
-                    <p>I am a very well rounded person and enjoy going to the gym, playing racquetball, mountain biking, riding my motorcycle, car audio, video games, poker, etc. etc.</p>
-                    <p class="updated">Page last updated May 18<sup>th</sup>, 2015</p>
+                    <p>I'm a UCF student (senior) finishing my undergrad degree in the summer of 2017 with a Bachelor of Science in Information Technologies (BSIT) and a minor in Secure Computing and Networking (SCAN). I am furthering my education with relevant certifications and professional development experience. After obtaining a full time job, I would like to return to academia and obtain a masters in Digital Forensics.</p>
+                    <p>Having lived in Florida my whole life, I will be venturing out to Arizona in the fall of 2017 to pursue my passion in security engineering. On top of being a Techranger, I do freelance work on the side including custom websites, security audits, and general IT work for various companies. Adaptability and hard work is the key to success and with my experiences I have what it takes to succeed.</p>
+                    <p>I am a very well-rounded person and participate in many hobbies. Most notably I am the president and one of the best players for the UCF Racquetball Team. I also partake in mountain biking with my rescue pup, so we are ecstatic for the challenging terrain of Arizona. Lastly, I enjoy solving puzzles due to the unforeseen challenges in cards, chess, and building things in the garage.</p>
+
+                    <p class="updated">Page last updated March 2017<sup>th</sup>, 2015</p>
                 </section>
             </section>
         </main>
