d9c896322b79c9813aacecb05179e342d97bc8bb
Adrian Goetz <adriangoetz@ucf.edu>
2017-03-24 11:31:02 -0400

    updated profile and profile pic

--- bio_adrian-goetz.php
+++ bio_adrian-goetz.php
@@ -25,16 +25,11 @@
                 <img src="images/bios/adrian.jpg" alt="Adrian Goetz" />
                 
                 <h2>Degree:</h2>
-                    <p>Computer Programming and Analysis</p>
+                    <p>Software Development</p>
                 
-                <h2>Expertise:</h2>
+                <h2>Technical Experience:</h2>
                     <ul>
                         <li>HTML 5</li>
-                        <li>Adobe Photoshop</li>
-                    </ul>
-                    
-                <h2>Learning:</h2>
-                    <ul>
                         <li>CSS 3</li>
                         <li>PHP</li>
                         <li>Python</li>
@@ -42,16 +37,14 @@
                         <li>C++ Programming</li>
                         <li>Java</li>
                         <li>Javascript</li>
+                        <li>Adobe Photoshop</li>
                     </ul>
                 
                 <h2>Biography:</h2>
-                    <p>I'm a Valencia student finishing my AS degree Computer Programming, I will then continue on to UCF to finish a 
-                        Bachelors degree in the same field.</p>
-                    <p>Originally from Colorado, I moved to florida in high school. I previously attended UCF and achieved a degree in Cinema Studies.
-                        I have since been working as a photographer and am now looking to get into web or application development.</p>
-                    <p>Working with the techrangers has been a great experience for me, giving me a place to learn from peers and implement the
-                        knowledge that I've gained.</p>
-                <p class="updated">Page last updated August 12<sup>th</sup>, 2014</p>
+                    <p>I'm a UCF student finishing my BAS degree in Software Development.</p>
+                    <p>Originally from Colorado, I moved to florida in high school. I previously attended UCF and achieved a degree in Cinema Studies. I have since steered my career towards web development.</p>
+                    <p>Working with the techrangers has been a great experience for me, giving me a place to learn from peers and implement the knowledge that I've gained.</p>
+                <p class="updated">Page last updated March 24<sup>th</sup>, 2017</p>
             </section>
         
         </section>
