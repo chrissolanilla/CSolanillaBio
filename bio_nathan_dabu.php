445fe8ec3b1292daa3d3af386a3a9ef0b4b4ec23
na876461 <nathan.dabu@ucf.edu>
2017-04-19 10:58:59 -0400

    added bio_nathan_dabu.php

---
+++ bio_nathan_dabu.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include('includes.html'); ?><!-- includes.html -->
+<title>Nathan Dabu - Techrangers</title>
+</head>
+
+<body>
+
+	<?php include('header.html'); ?> <!-- header.html -->
+	
+	<main role="main" id="biomain">
+		<!-- main content -->
+		<div id="title">
+			<h1>Meet the Techrangers</h1> 
+		</div>
+		
+		<?php include('sidebar.html'); ?> <!-- sidebar.html -->
+		
+		<section role="region" class="pagecontent" id="biopage">
+			
+			<section role="region">
+			<h1 class="bioh1">Nathan Dabu</h1>
+		
+				<img src="images/bios/nathan.jpg" alt="Nathan Dabu" />
+				
+				<h2>Degree:</h2>
+					<p>Computer Science</p>
+                
+                <h2>Expertise:</h2>
+					<ul>
+                        <li>C</li>
+                        <li>Java</li>
+                        <li>Adobe Photoshop</li>
+					</ul>
+				<h2>Learning:</h2>
+					<ul>
+						<li>JavaScript</li>
+						<li>Python</li>
+                        <li>HTML 5</li>
+                        <li>CSS</li>
+					</ul>
+				
+				<h2>Biography:</h2>
+					<p>
+						I'm a Computer Science student at the University of Central Florida. I started with the Techrangers in April of 2017.
+					</p>
+                    <p>
+                        I'm from Merritt Island, Florida, which is just an hour East of UCF. I started programming some Java during highschool, and I've expanded to several other langauges, currently focusing on web development with the Techrangers.
+                    </p>
+			
+				<p class="updated">Page last updated April 20<sup>th</sup>, 2017</p>
+			</section>
+		
+		</section>
+	</main>
+	
+	<?php include('footer.html'); ?> <!-- footer.html -->
+
+</body>
+</html>
