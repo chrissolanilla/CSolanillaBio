98de4f19d74305f69965db0552613bc9c48c1b0b
Keegan Berry <kberry@cdl215350.local>
2017-11-01 17:42:45 -0400

    update past-bios content

--- past_keegan-berry.php
+++ past_keegan-berry.php
@@ -8,52 +8,53 @@
 <body>
 
     <?php include('header.html'); ?> <!-- header.html -->
-    
+
     <main role="main" id="biomain">
     	<!-- main content -->
         <div id="title">
-            <h1>Meet the Techrangers</h1> 
+            <h1>Meet the Techrangers</h1>
         </div>
-        
+
         <?php include('sidebar.html'); ?> <!-- sidebar.html -->
-        
+
         <section role="region" class="pagecontent" id="biopage">
-        
+
             <section role="region">
             <h1 class="bioh1">Keegan Berry</h1>
-        
+
             	<img src="images/bios/keegan.jpg" alt="Keegan Berry" />
-            	
+
                 <h2>Degree:</h2>
                 	<p>Digital Media</p>
 
                 <h2>Website:</h2>
                     <p><a href="http://keeganberry.com/" target="_blank">http://keeganberry.com/</a></p>
-                
+
                 <h2>Expertise:</h2>
                 	<ul>
-                        <li>(X)HTML</li>
-                        <li>CSS</li>
-                        <li>Javascript and jQuery</li>
-                        <li>Photoshop</li>
-                    </ul>
-                    
-                <h2>Not Exactly an Expert with:</h2>
-                	<ul>
+                        <li>HTML</li>
                         <li>PHP</li>
-                        <li>Actionscript, Flash, and Flex</li>
+                        <li>CSS/SCSS</li>
+                        <li>JavaScript</li>
+                        <li>Adobe<sup>&reg;</sup> Photoshop<sup>&reg;</sup></li>
                     </ul>
-                
+
+                <h2>Learning:</h2>
+                	<ul>
+                        <li>Python &amp; Flask</li>
+                        <li>Adobe<sup>&reg;</sup> Illustrator<sup>&reg;</sup></li>
+                    </ul>
+
                 <h2>Biography:</h2>
-                	<p>I'm a graduate of the University of Central Florida, with a BA in Digital Media. The idea of the DM program appealed to me because it suggested a good mix of design and development. Not that surprisingly, I enjoy working on projects that incorporate some kind of graphical or visual component. I also like movies, drawing/illustration, cooking, bikes, and I’m trying to get into photography.</p>
+                	<p>I'm a graduate of the University of Central Florida, with a B.A. in Digital Media. I enjoy working on projects that incorporate some kind of graphical or visual component. I also enjoy movies, design, photography and occasionally cooking.</p>
                     <p class="rooster"></p>
-        	
+
                 <p class="updated">Page last updated January 29<sup>th</sup>, 2014</p>
             </section>
-        
+
         </section>
     </main>
-    
+
 	<?php include('footer.html'); ?> <!-- footer.html -->
 </body>
 </html>
