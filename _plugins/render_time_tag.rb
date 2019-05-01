eca2b3ef809e4475ef5ed2ab12088ca9ef0edec7
Ian Turgeon <iturgeon@gmail.com>
2019-04-30 23:36:25 -0400

    adds average tenure calculation

--- _plugins/render_time_tag.rb
+++ _plugins/render_time_tag.rb
@@ -1,5 +1,6 @@
-require 'set'
+require 'date'
 Jekyll::Hooks.register :site, :post_read do |site|
+
   degrees = []
   degree_map = {}
 
@@ -10,7 +11,8 @@ Jekyll::Hooks.register :site, :post_read do |site|
   genders = []
   gender_map = {}
 
-  full_timers = []
+  total_dates = 0
+  dates_sum = 0
 
   techrangers = site.collections['techrangers_past'].docs | site.collections['techrangers_current'].docs
   techrangers.each do |person|
@@ -57,19 +59,28 @@ Jekyll::Hooks.register :site, :post_read do |site|
       thisGender['count'] += 1
     end
 
-    # collect fullTimers
-    if person.data['full_time']
-      full_timers << person
+    # collect tenure
+    if person.data['date'] && person.data['resigned'] && person.data['resigned'] != '??'
+      if person.data['resigned'].kind_of? String
+        person.data['resigned'] = Date.strptime(person.data['resigned'], '%Y-%m-%d')
+      end
+      start_d = Date.parse(person.data['date'].to_s)
+      end_d = person.data['resigned']
+      total_dates = total_dates + 1
+      days_diff = end_d - start_d
+      # puts days_diff
+      dates_sum = dates_sum + days_diff
     end
 
   end
+
+  dates_av_days = dates_sum / total_dates
 
   jobs.sort_by! { |a| a['people'].size }
   degrees.sort_by! { |a| a['people'].size }
   genders.sort_by! { |a| a['count'] }
 
-
-  site.config['tr'] = {'tr_count' => techrangers.size, 'jobs' => jobs.reverse!, 'total_jobs' => total_jobs, 'degrees' => degrees.reverse!, 'genders' => genders.reverse}
+  site.config['tr'] = {'dates_av_days' => dates_av_days.to_i, 'tr_count' => techrangers.size, 'jobs' => jobs.reverse!, 'total_jobs' => total_jobs, 'degrees' => degrees.reverse!, 'genders' => genders.reverse}
 end
 
 
@@ -78,15 +89,22 @@ module ArraySlice
     input.slice(start, length)
   end
 
-  def combine_some_into(input, count_param, compare, compare_to, title)
-    compare_to = compare_to.to_i
+  # given an array, compare a paramater of each array hash given an operator
+  # all items that evaluate to false will be hidden in an "others" item
+  # and appended to the end of the results as others
+  # EX {% assign tall_people = array_of_people_hashes | combine_some_into: height, >, 5, 'below 5 feet' %}
+  # given an array of hashes like { 'title' => 'Ian', 'height' => 6 }
+  # # all items with a hight above 5 will be returned unaltered
+  # any item in the array with a key 'height' of a value '>' compared to '5' will be consolidated a single item at the end of the array
+  def combine_some_into(input, count_param, operator, compare_to, title)
+    compare_to = compare_to
     newOutput = []
     others = { 'title' => title }
     others[count_param] = []
 
     input.each do |i|
       count = i[count_param].size
-      if count.send(compare, compare_to)
+      if count.send(operator, compare_to)
         newOutput << i
       else
         others[count_param] += i[count_param]
