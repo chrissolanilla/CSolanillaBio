054bc30920a342f6a707bc0714b58b999bd403f5
Jacob Bates <jacob.bates@ucf.edu>
2022-06-08 10:52:42 -0400

    Fixed comment at top of docker_run

--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# This script builds the docker image for the Techrangers site so that it can be
-# launched as a container in the docker run script.
+# This script runs the docker container for the Techrangers site usign the docker
+# image you created earlier.
 
 echo "Running site.  Press Ctrl+C to quit."
 echo "docker run --rm -p 4000:4000 -p 35729:35729 --platform linux/amd64 -v "$(pwd)":/website ruby-tr-site-build-container bundle exec rake dev_docker"
